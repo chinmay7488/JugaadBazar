1127881a35e29670f28eb6feab6702835c83a310
chinmay7488 <chinmayjain8314@gmail.com>
2024-07-27 01:00:45 +0530

    Fixing Static

--- build_files.sh
+++ build_files.sh
@@ -3,5 +3,5 @@ python3.9 -m ensurepip --user
 python3.9 -m pip install -r requirements.txt
 python3.9 manage.py makemigration
 python3.9  manage.py migrate
-python3.9  manage.py collectstatic
+python3.9  manage.py collectstatic --noinput
 echo "BUILD END"
