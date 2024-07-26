5d70d925b1e17c3e8bce2b803b901adc8e88d653
chinmay7488 <chinmayjain8314@gmail.com>
2024-07-27 01:34:01 +0530

    Fixed cursor

--- build_files.sh
+++ build_files.sh
@@ -3,5 +3,6 @@ python3.9 -m ensurepip --user
 python3.9 -m pip install -r requirements.txt
 python3.9 manage.py makemigration
 python3.9  manage.py migrate
+python3.9 manage.py createsuperuser 9589445122 1234
 python3.9  manage.py collectstatic --noinput
 echo "BUILD END"
