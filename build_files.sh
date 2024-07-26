414e770c79ae4a52890f0694e30a56490b2ff8ad
chinmay7488 <chinmayjain8314@gmail.com>
2024-07-27 00:12:29 +0530

    made some changes

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,5 @@
 echo "BUILD START"
-pip install -r reqirements.txt
+python3.9 pip install -r reqirements.txt
 python3.9 manage.py makemigration
 python3.9  manage.py migrate
 python3.9  manage.py collectstatic
