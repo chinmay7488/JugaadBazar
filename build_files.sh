add82985389b3de8b3bd05bdd852a4a9ef956230
chinmay7488 <chinmayjain8314@gmail.com>
2024-07-26 23:55:26 +0530

    fixed some issues for deloyment

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,4 @@
 echo "BUILD START"
-python3.11 -m pip install -r reqirements.txt
-python3.11 manage.py collectstatic
+python -m pip install -r reqirements.txt
+python manage.py collectstatic
 echo "BUILD END"
