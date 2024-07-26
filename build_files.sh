448cebbe6530821f1416db754e03184f45e255ee
Chinmay Jain <51847447+chinmay7488@users.noreply.github.com>
2024-07-27 01:59:00 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,8 +1,5 @@
 echo "BUILD START"
 python3.9 -m ensurepip --user
 python3.9 -m pip install -r requirements.txt
-python3.9 manage.py makemigration
-python3.9  manage.py migrate
-python3.9 manage.py createsuperuser 9589445122 1234
 python3.9  manage.py collectstatic --noinput
 echo "BUILD END"
